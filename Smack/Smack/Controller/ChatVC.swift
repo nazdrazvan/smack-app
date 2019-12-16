807cc9084d31ea03dbe7779763376320cb787efc
razvanmorea <razvanmorea@yahoo.ro>
2019-12-16 13:06:59 +0200

    gradient and added constraints for rear vc

---
+++ Smack/Smack/Controller/ChatVC.swift
@@ -0,0 +1,30 @@
+//
+//  ChatVC.swift
+//  smack
+//
+//  Created by Razvan Morea on 16/12/2019.
+//  Copyright © 2019 Razvan Morea. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Smack/Smack/View/GradientVIew.swift
@@ -0,0 +1,21 @@
+//
+//  GradientVIew.swift
+//  smack
+//
+//  Created by Razvan Morea on 16/12/2019.
+//  Copyright © 2019 Razvan Morea. All rights reserved.
+//
+
+import UIKit
+
+class GradientVIew: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
